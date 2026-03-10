087f2a657a7cb3f09b80592946bb55149810277a
Khanh <plk4122004@gmail.com>
2026-03-11 02:19:50 +0700

    fix: clear order-related session data, modify the session service

--- Services/Session/ISessionService.cs
+++ Services/Session/ISessionService.cs
@@ -7,6 +7,8 @@ namespace MediaStore.Services.Session
     {
         T Get<T>(string key);
         void Set<T>(string key, T value);
+
+        void Remove(string key);
         void Clear();
     }
 }
